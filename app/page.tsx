c8f431e796140f61ab280f5b4ba4dcc8df04d178
ethan-heo <ethan.hb94@gmail.com>
2023-08-23 23:22:19 +0900

    chore: prettier를 적용한다

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
-import { NotionModule } from "@adapter/notion.module";
 import getConfig from "next/config";
 import Image from "next/image";
+
+import { NotionModule } from "@adapter/notion.module";
 
 export default async function Home() {
   const config = getConfig();
 
   const databases = await NotionModule.getNotionDatabases({
-    database_id: process.env.NOTION_DATABASE_ID as string
-  })
+    database_id: process.env.NOTION_DATABASE_ID as string,
+  });
 
-  console.log(JSON.stringify(databases, null, 2))
+  console.log(JSON.stringify(databases, null, 2));
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing????&nbsp;
-          ${JSON.stringify(config)}
+          Get started by editing????&nbsp; ${JSON.stringify(config)}
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
