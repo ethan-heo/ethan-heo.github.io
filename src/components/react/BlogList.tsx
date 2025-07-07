d3e46d12af6baef77135702c109caf01a1eb10f4
ethan-heo <ethan.hb94@gmail.com>
2025-07-07 15:36:29 +0900

    style: header와 컨텐츠 영역에 공백을 추가한다

--- src/components/react/BlogList.tsx
+++ src/components/react/BlogList.tsx
@@ -11,7 +11,7 @@ function BlogList() {
     const blogs = blogData.slice(0, sliceSize);
 
     return (
-        <div className="flex flex-col gap-5 items-center">
+        <div className="flex flex-col gap-5 items-center mt-20">
             <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
                 {blogs.map((blog) => (
                     <li
