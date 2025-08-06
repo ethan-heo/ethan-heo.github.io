eaf0997e4b4224437e8e34c0c661982449f4ba63
ethan-heo <ethan.hb94@gmail.com>
2025-08-07 08:54:00 +0900

    feat: blog service 어댑터를 구현한다

--- v1/blog/service/index.ts
+++ v1/blog/service/index.ts
@@ -1,1 +1,20 @@
-interface BlogService {}
+import type { BlogDomain } from "../domain/interfaces/domain.interface";
+import type { BlogRepository } from "../repository/repository.interface";
+import {
+    getBlogContentsUseCase,
+    getBlogListUseCase,
+    searchBlogItemsUseCase,
+    transformBlogListToJSONUseCase,
+} from "./use-cases";
+
+const BlogService = (domain: BlogDomain, repository: BlogRepository) => ({
+    transformBlogListToJSONUseCase: transformBlogListToJSONUseCase(
+        domain,
+        repository,
+    ),
+    getBlogContentsUseCase: getBlogContentsUseCase(domain, repository),
+    getBlogListUseCase: getBlogListUseCase(domain, repository),
+    searchBlogItemsUseCase: searchBlogItemsUseCase(domain, repository),
+});
+
+export default BlogService;
