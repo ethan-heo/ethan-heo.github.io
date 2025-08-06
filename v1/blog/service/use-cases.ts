6406777e682ef7166a4f714f3e3a536da5a9ddaa
ethan-heo <ethan.hb94@gmail.com>
2025-08-07 08:50:48 +0900

    feat: use-case 네이밍을 변경한다

--- v1/blog/service/use-cases.ts
+++ v1/blog/service/use-cases.ts
@@ -5,33 +5,34 @@ import type {
 } from "../domain/interfaces/model.interface";
 import type { BlogUseCase } from "./use-case.interface";
 
-type TransformBlogListToJSON = BlogUseCase<[string, string], void>;
+type TransformBlogListToJSONUseCase = BlogUseCase<[string, string], void>;
 
-export const transformBlogListToJSON: TransformBlogListToJSON =
+export const transformBlogListToJSONUseCase: TransformBlogListToJSONUseCase =
     (domain, repository) => async (id, target) => {
         const blogList = await repository.getOriginalBlogList(id);
 
         repository.toJSON(domain.transformBlogItems(blogList), target);
     };
 
-type GetBlogContents = BlogUseCase<[string], Promise<BlogContent[]>>;
+type GetBlogContentsUseCase = BlogUseCase<[string], Promise<BlogContent[]>>;
 
-export const getBlogContents: GetBlogContents =
+export const getBlogContentsUseCase: GetBlogContentsUseCase =
     (domain, repository) => async (id) => {
         const originalContents = await repository.getOriginalContents(id);
 
         return originalContents.map(domain.transformOriginalBlogContent);
     };
 
-type GetBlogList = BlogUseCase<[number, number], BlogItem[]>;
+type GetBlogListUseCase = BlogUseCase<[number, number], BlogItem[]>;
 
-export const getBlogList: GetBlogList = (_, repository) => (page, size) => {
-    return repository.getBlogList(page, size);
-};
+export const getBlogListUseCase: GetBlogListUseCase =
+    (_, repository) => (page, size) => {
+        return repository.getBlogList(page, size);
+    };
 
-type SearchBlogItems = BlogUseCase<[string], SearchedBlogItem[]>;
+type SearchBlogItemsUseCase = BlogUseCase<[string], SearchedBlogItem[]>;
 
-export const searchBlogItems: SearchBlogItems =
+export const searchBlogItemsUseCase: SearchBlogItemsUseCase =
     (domain, repository) => (searchQuery) => {
         domain.validateSearchQuery(searchQuery);
 
