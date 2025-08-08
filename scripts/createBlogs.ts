57864a1b9d9a60757b7f8f89120961b8b815da4c
ethan-heo <ethan.hb94@gmail.com>
2025-08-08 15:22:14 +0900

    refactor: 컨트롤러 로직을 어댑터 처럼 활용하도록 변경한다

--- scripts/createBlogs.ts
+++ scripts/createBlogs.ts
@@ -1,20 +1,16 @@
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import createBlogController from "../v1/blog/controller.ts";
-import createNotionAPI from "../v1/blog/apis/notion.ts";
-import createBlogAPI from "../v1/blog/apis/blog.ts";
+import blogController from "../v1/blog/controller.ts";
 
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const { NOTION_API_KEY, NOTION_DATABASE_ID } = process.env;
-const controller = createBlogController(createNotionAPI(), createBlogAPI());
 
-controller.notion.init(NOTION_API_KEY as string);
-
-controller.blog.createBlogListToJSON(
-    await controller.notion.getBlogItemAll(NOTION_DATABASE_ID as string),
+blogController.notion.init(NOTION_API_KEY as string);
+blogController.blog.createBlogListToJSON(
+    await blogController.notion.getBlogItemAll(NOTION_DATABASE_ID as string),
     path.resolve(__dirname, "../src/assets/blogs.json"),
 );
--- v1/blog/controller.ts
+++ v1/blog/controller.ts
@@ -1,34 +1,29 @@
 import blogDomain from "./domain/index.ts";
 import createBlogRepository from "./repository/index.ts";
-import type { NotionAPI } from "./apis/notion.ts";
-import type { BlogAPI } from "./apis/blog.ts";
 import createBlogService from "./service/index.ts";
 import type { BlogItem } from "./domain/interfaces/model.interface.ts";
+import createNotionAPI from "./apis/notion.ts";
+import createBlogAPI from "./apis/blog.ts";
 
-const createBlogController = (notionAPI: NotionAPI, blogAPI: BlogAPI) => {
-    const service = createBlogService(
-        blogDomain,
-        createBlogRepository(notionAPI, blogAPI),
-    );
+const service = createBlogService(
+    blogDomain,
+    createBlogRepository(createNotionAPI(), createBlogAPI()),
+);
 
-    return {
-        notion: {
-            init: (apiKey: string) => service.initNotionClientUseCase(apiKey),
-            getBlogContentAll: (id: string) =>
-                service.getBlogContentAllUseCase(id),
-            getBlogItemAll: (id: string) => service.getBlogItemAllUseCase(id),
-        },
-        blog: {
-            createBlogListToJSON: (
-                originalBlogItems: BlogItem[],
-                target: string,
-            ) => service.createBlogListToJSONUseCase(originalBlogItems, target),
-            getBlogListFromJSON: (page: number, size: number) =>
-                service.getBlogListFromJSONUseCase(page, size),
-            searchBlogItems: (searchQuery: string) =>
-                service.searchBlogItemsUseCase(searchQuery),
-        },
-    };
+const blogController = {
+    notion: {
+        init: (apiKey: string) => service.initNotionClientUseCase(apiKey),
+        getBlogContentAll: (id: string) => service.getBlogContentAllUseCase(id),
+        getBlogItemAll: (id: string) => service.getBlogItemAllUseCase(id),
+    },
+    blog: {
+        createBlogListToJSON: (originalBlogItems: BlogItem[], target: string) =>
+            service.createBlogListToJSONUseCase(originalBlogItems, target),
+        getBlogListFromJSON: (page: number, size: number) =>
+            service.getBlogListFromJSONUseCase(page, size),
+        searchBlogItems: (searchQuery: string) =>
+            service.searchBlogItemsUseCase(searchQuery),
+    },
 };
 
-export default createBlogController;
+export default blogController;
